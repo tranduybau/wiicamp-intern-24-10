2998ace994fec2d2509085476602e6c20d14496a
tranduybau <trdbau@gmail.com>
2023-10-24 09:30:51 +0700

    [task] add eslint and fix eslint's issues

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
-import '@/styles/globals.css'
+import React from "react";
+import PropTypes from "prop-types";
+
+import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
+
+App.propTypes = {
+  Component: PropTypes.instanceOf(Object).isRequired,
+  pageProps: PropTypes.instanceOf(Object).isRequired,
+};
