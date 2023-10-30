c391bcf6ddc8cad312c6bd0e11447a08a3c8a153
Chu Phong <man.nguyen.cit@gmail.com>
2023-10-30 09:46:57 +0700

    update login

--- pages/_document.js
+++ pages/_document.js
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/google-font-display */
 import React from "react";
 import { Head, Html, Main, NextScript } from "next/document";
 
@@ -5,9 +6,9 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link href="https://fonts.googleapis.com/css?family=Poppins&display=optional" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet" />
 
-        <link href="https://fonts.googleapis.com/css?family=Inter&display=optional" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Inter" rel="stylesheet" />
       </Head>
       <body>
         <Main />
