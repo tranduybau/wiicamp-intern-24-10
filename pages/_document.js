a6bd17d9e8abccec8e6d092d89614e154ec9b488
Chu Phong <man.nguyen.cit@gmail.com>
2023-10-30 09:32:51 +0700

    update login

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,3 @@
-/* eslint-disable @next/next/google-font-display */
 import React from "react";
 import { Head, Html, Main, NextScript } from "next/document";
 
@@ -6,16 +5,9 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        {/* <link
-          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
-          rel="stylesheet"
-          integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
-          crossOrigin="anonymous"
-        /> */}
+        <link href="https://fonts.googleapis.com/css?family=Poppins&display=optional" rel="stylesheet" />
 
-        <link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet" />
-
-        <link href="https://fonts.googleapis.com/css?family=Inter" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Inter&display=optional" rel="stylesheet" />
       </Head>
       <body>
         <Main />
